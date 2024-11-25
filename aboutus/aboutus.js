37102fb41f404779971fa02a6a25ccb50ad2277e
Aniket3689 <aniketkumar89566@gmail.com>
2024-11-25 15:37:27 +0530

    contactus page add

---
+++ aboutus/aboutus.js
@@ -0,0 +1,14 @@
+// Smooth Scrolling for Sidebar Links
+document.querySelectorAll('.sidebar a').forEach(link => {
+    link.addEventListener('click', function (e) {
+      e.preventDefault();
+      const targetId = this.getAttribute('href').substring(1);
+      const targetElement = document.getElementById(targetId);
+  
+      targetElement.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+      });
+    });
+  });
+  
