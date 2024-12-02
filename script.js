46a4ebfa132c66814b0c484ee9386fe938fb4454
Aniket3689 <aniketkumar89566@gmail.com>
2024-12-02 15:00:50 +0530

    contactus page add

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 
   const sliderImages = [
+    { src: "./image/Roomfreshner1.png", description: "Efficient and eco-friendly solutions." },
+
     { src: "./image/Heroimage.png", description: "High-quality cleaning products for your home." },
     { src: "./image/homw wash.jpg", description: "Efficient and eco-friendly solutions." },
   ];
